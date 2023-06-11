6cfc3adc18e2acbfe8fea1cdec3cdf04a26001c1
Angathan003 <angathan003@gmail.com>
2023-06-11 19:58:49 +0530

    Using for loop

--- main.py
+++ main.py
@@ -6,29 +6,26 @@ if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
     if top_of_range <= 0:
-        print("please type a number larger than 0 next time: ")
+        print("Please type a number larger than 0 next time.")
         quit()
-
 else:
-    print("Please type a number next time")
+    print("Please type a number next time.")
     quit()
 
 random_number = random.randint(0, top_of_range)
-guess = 0
-while True:
-    guess += 1
-    user_guess = input("Make a Guess: ")
+
+for guess in range(1, top_of_range + 1):
+    user_guess = input("Make a guess: ")
+
     if user_guess.isdigit():
         user_guess = int(user_guess)
-    else:
-        print("Please type a number next time.: ")
-        continue
-    if user_guess == random_number:
-        print("You got it")
-        break
-    elif user_guess > random_number:
-        print("You were above the number!")
-    else:
-        print("You were below the number!")
+        result = "You were above the number!" if user_guess > random_number else "You were below the number!"
 
-print("You got it in", guess, "guess")
+        if user_guess == random_number:
+            print("You got it")
+            print("You got it in", guess, "guess")
+            break
+        else:
+            print(result)
+    else:
+        print("Please type a number next time.")
